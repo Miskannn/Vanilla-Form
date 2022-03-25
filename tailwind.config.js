f2c8585b4c885b0da8a24c1c4afaad630c4200de
Miskannn <maksim.kovtun2103@gmail.com>
2022-03-25 19:51:29 +0200

    layout_fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./index.html","./style.css","./**/*.{js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          50: '#5ab98c',
+        },
+        gray: {
+          50: '#8e8e93',
+          10: '#f5f5f5',
+        },
+        red: {
+          50: '#d2877d',
+        },
+      },
+    },
   },
   plugins: [],
 }
