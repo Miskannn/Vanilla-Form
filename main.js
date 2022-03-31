46d4b9811b1bce1480741e196c7e1be007f97504
Miskannn <maksim.kovtun2103@gmail.com>
2022-03-31 14:50:25 +0300

    modernization

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-import {validInput} from "./modules/validation";
+// import {validInput} from "./modules/validation";
+import './style.css'
+import { validInput } from './modules/validation'
 
 const form = document.forms?.main;
-const email = document.getElementById("email");
+const header = document.getElementById("header");
+const email = document.getElementById('email');
 const password = document.getElementById("password");
-const eye = document.querySelector("svg[data-icon='eye']")
-const eyeOff = document.querySelector("svg[data-icon='eye-off']")
+const eye = document.querySelector("svg[data-icon='eye']");
+const eyeOff = document.querySelector("svg[data-icon='eye-off']");
+
 
 const preventSubmit = e => {
     e.preventDefault();
-    if(email.value === "admin@gmail.com" && password.value === "123456") alert("Success submit");
-    else {
-        alert("You are not authorized")
+    const emailValidity = email?.validity?.valid && email.value.trim();
+    const passwordValidity = password?.validity?.valid && password.value.trim();
+    const wrongPassword = emailValidity && !passwordValidity;
+
+    if(email.value === "admin@gmail.com" && password.value === "123456"){
+        alert("Success submit");
+    } else {
+        alert("You are not authorized");
     }
+
+    header.textContent =
+      emailValidity && passwordValidity
+        ? 'Welcome!'
+        : wrongPassword
+          ? 'Wrong password'
+          : 'Oops!';
+
     password.value = '';
     email.value = '';
 };
 
-form.addEventListener('submit', preventSubmit);
-form.addEventListener('input', validInput);
 
-
-eye.addEventListener('click', () => password.setAttribute("type","password"));
-eyeOff.addEventListener('click', () => password.setAttribute("type","text"));
+eye.addEventListener('click',()=>password.setAttribute('type', 'password'));
+form.addEventListener('submit', preventSubmit)
+eyeOff.addEventListener('click',()=>password.setAttribute('type', 'text'));
